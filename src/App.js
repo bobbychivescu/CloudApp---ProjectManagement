ccdd05e15e67c0422a4754fbe65d1ae3cff9b6ca
Bogdan Chivescu <bac1g16@soton.ac.uk>
2018-11-03 02:03:01 +0000

    starting the real backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Amplify, {API} from 'aws-amplify';
 import aws_exports from './aws-exports';
@@ -9,24 +9,32 @@ Amplify.configure(aws_exports);
 
 class App extends Component {
 
+    ID = 1;
     post = async () => {
         console.log('calling api');
-        const response = await API.post('lambdas', '/items', {
+        const response = await API.post('projectsCRUD', '/projects', {
             body: {
-                ID: '1',
-                status: 'new'
+                ID: String(this.ID++),
+                status: 'new',
+                manager: 'someone',
+                developers: ['john', 'bob']
             }
         });
         alert(JSON.stringify(response, null, 2));
     }
     get = async () => {
         console.log('calling api');
-        const response = await API.get('lambdas', '/items/object/1');
+        const response = await API.get('projectsCRUD', '/projects/object/1');
         alert(JSON.stringify(response, null, 2));
     }
     list = async () => {
         console.log('calling api');
-        const response = await API.get('lambdas', '/items/1');
+        const response = await API.get('projectsCRUD', '/projects');
+        alert(JSON.stringify(response, null, 2));
+    }
+    del = async (ID) => {
+        console.log('calling api');
+        const response = await API.del('projectsCRUD', '/projects/object/1');
         alert(JSON.stringify(response, null, 2));
     }
 
@@ -38,6 +46,7 @@ class App extends Component {
             <button onClick={this.post}>POST</button>
             <button onClick={this.get}>GET</button>
             <button onClick={this.list}>LIST</button>
+            <button onClick={this.del}>LIST</button>
         </header>
       </div>
     );
